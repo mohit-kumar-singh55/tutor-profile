8dc5d0c74b7c340d8bd0e9a8d7240202b4860ccc
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-04-07 10:22:13 +0530

    created video component

--- components/TutorProfile/AboutTutor.js
+++ components/TutorProfile/AboutTutor.js
@@ -8,9 +8,9 @@ function AboutTutor() {
       <TopTitle />
 
       {/* mainContainer */}
-      <main>
+      <main className='flex items-center justify-center flex-wrap-reverse gap-5'>
         <Video />
-
+        <Description />
       </main>
     </div>
   )
@@ -44,8 +44,8 @@ function SmallLine() {
 function Video(props) {
   // overflow-hidden  rounded-xl bg-gradient-to-r from-[#FD4E6D] to-[#FDA02F] p-1
   return (
-    <div className=" mx-auto mb-6 flex w-full flex-col gap-4 sm:w-[376px] md:w-auto ">
-      <div className="  md:white-linear-gradient h-[161px] rounded-lg bg-gradient-to-r  from-[#FD4E6D] to-[#FDA02F]  p-1.5  md:h-[486px] md:w-[430px] md:rounded-xl  ">
+    <div className=" mx-auto mb-6 flex w-full flex-col gap-4 sm:w-[467px] md:w-auto ">
+      <div className="  md:white-linear-gradient h-[161px] rounded-lg bg-gradient-to-r  from-[#FD4E6D] to-[#FDA02F]  p-1.5 sm:h-[302px] sm:w-[467px] md:rounded-xl  ">
         <div className="relative order-2 flex h-full w-full  items-center justify-center   overflow-hidden   rounded-xl  md:order-1     ">
           {/* <div className="relative order-2 flex items-center justify-center   overflow-hidden   rounded-xl  md:order-1 md:h-[486px] md:w-[430px]    "> */}
           {/* <Image
@@ -57,7 +57,7 @@ function Video(props) {
 
           <div className="white-linear-gradient absolute  bottom-0" />
           <Link href={'#'}>
-            <a className=" relative z-10 h-16 w-16 transition-all delay-150  ease-in-out hover:scale-[1.2] md:h-24 md:w-24">
+            <a className=" relative z-10 h-16 w-16 transition-all delay-150  ease-in-out hover:scale-[1.2] md:h-[76.67px] md:w-[76.67px]">
               <Image
                 src={'/Images/TutorProfile/svg/video-play-btn.svg'}
                 objectFit="cover"
@@ -66,7 +66,7 @@ function Video(props) {
             </a>
           </Link>
 
-          <div className="absolute  bottom-[18px] hidden flex-col  justify-center gap-[14px] md:flex">
+          <div className="absolute  bottom-[15px] hidden flex-col  justify-center gap-[14px] md:flex">
             <div className=" flex justify-center gap-2 ">
               {Array.from(Array(5), (index) => index + 1).map((index) => (
                 <Image
